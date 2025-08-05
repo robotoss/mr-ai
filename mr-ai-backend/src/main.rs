5c8659c5b3db83859e7c9ebeefd3f8b2494d532b
Konstantin <kos@Mac.fritz.box>
2025-08-05 22:47:14 +0300

    feat: Init API lib

--- mr-ai-backend/src/main.rs
+++ mr-ai-backend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use std::error::Error;
+
+use api;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+
+    api::start();
+
+    Ok(())
 }
